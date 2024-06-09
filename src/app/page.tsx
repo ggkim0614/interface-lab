5b3e63a37f68f90c0348638303db77649c2eece8
George Kim <ggkim0614@gmail.com>
2024-06-09 04:13:37 -0700

    Add animatepresence

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import { Button } from '@/components/ui/button'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Bell, Bookmark, SquarePen } from 'lucide-react'
-import { motion } from 'framer-motion'
+import { motion, AnimatePresence } from 'framer-motion'
 import { useState } from 'react'
 
 export default function Home() {
@@ -22,33 +22,47 @@ export default function Home() {
             style={
               isToggled ? { flexDirection: 'column' } : { flexDirection: 'row' }
             }
-            className="flex items-center gap-8 rounded-[24px] border-[1px] border-gray-200 bg-white p-5"
+            className="flex items-center gap-8 rounded-[24px] border-[1px] border-gray-200 bg-white p-5 shadow-md"
           >
-            <Avatar className="w-[40px]">
-              <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-            <Button variant="ghost" size="icon">
-              <Bookmark className="w-6" />
-            </Button>
-            <Button variant="ghost" size="icon">
-              <Bell className="w-6" />
-            </Button>
-            <Button
-              onClick={handleClick}
-              className="rounded-[12px] text-[16px] font-semibold"
-            >
-              <SquarePen
-                className="w-4 opacity-60"
-                style={isToggled ? { marginRight: 0 } : { marginRight: 6 }}
-              />
-              <motion.div
-                layout
-                style={isToggled ? { display: 'none' } : { display: 'block' }}
+            <motion.div layoutId="avatar">
+              <Avatar className="w-[40px]">
+                <AvatarImage
+                  src="https://github.com/shadcn.png"
+                  alt="@shadcn"
+                />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </motion.div>
+            <motion.div layoutId="bookmark">
+              <Button variant="ghost" size="icon">
+                <Bookmark className="w-6" />
+              </Button>
+            </motion.div>
+            <motion.div layoutId="notif">
+              <Button variant="ghost" size="icon">
+                <Bell className="w-6" />
+              </Button>
+            </motion.div>
+            <motion.div layoutId="button">
+              <Button
+                onClick={handleClick}
+                className="rounded-[12px] text-[16px] font-semibold"
               >
-                Write
-              </motion.div>
-            </Button>
+                <SquarePen
+                  className="w-4 opacity-60"
+                  style={isToggled ? { marginRight: 0 } : { marginRight: 6 }}
+                />
+                <AnimatePresence>
+                  <div
+                    style={
+                      isToggled ? { display: 'none' } : { display: 'block' }
+                    }
+                  >
+                    Write
+                  </div>
+                </AnimatePresence>
+              </Button>
+            </motion.div>
           </motion.div>
         </div>
       </section>
