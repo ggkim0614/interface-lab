4aed11b6ab558322a3c0ae07cb2ddfcff3ac4410
George Kim <ggkim0614@gmail.com>
2024-06-21 16:35:51 -0700

    add search-input ver2

--- src/components/sections/search-input.tsx
+++ src/components/sections/search-input.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useRef } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { Search } from 'lucide-react'
 import { cn } from '@/lib/utils'
 import Section from './section'
@@ -11,11 +11,28 @@ import { AnimatePresence, motion } from 'framer-motion'
 export default function SearchInput() {
   const [isToggled, setIsToggled] = useState(false)
   const inputRef = useRef(null)
-  console.log(inputRef)
 
-  const kbdVariants = {
-    hidden: { opacity: 0, scale: 0.5 },
-    visible: { opacity: 1, scale: 1 },
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === 'k' && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault()
+        focusInput()
+      } else if (e.key === 'Escape') {
+        e.preventDefault()
+        console.log('escape')
+        blurInput()
+      }
+    }
+    document.addEventListener('keydown', down)
+    return () => document.removeEventListener('keydown', down)
+  }, [])
+
+  const focusInput = () => {
+    inputRef.current.focus()
+  }
+
+  const blurInput = () => {
+    inputRef.current.blur()
   }
 
   return (
@@ -23,35 +40,98 @@ export default function SearchInput() {
       <Section
         title="Search Input"
         description="temp description for search input"
-        labels={['hello', 'world', 'this', 'is', 'test']}
+        labels={['React', 'Framer Motion', 'TailwindCSS']}
         frameHeight={400}
       >
-        <div className="relative flex ">
-          <motion.input
-            layout
-            ref={inputRef}
-            type="text"
-            className="h-10 w-[200px] rounded-[14px] border-[3px] border-gray-200 bg-gray-100 px-3 text-sm font-medium text-gray-800 transition-all ease-out placeholder:text-gray-400 focus:w-[400px] focus:outline-none focus:ring-[3px] focus:ring-gray-700 focus:placeholder:opacity-50"
-            placeholder="Search item"
-            onFocus={() => setIsToggled(!isToggled)}
-            onBlur={() => setIsToggled(!isToggled)}
-          ></motion.input>
-          <span className="absolute inset-y-0 right-0 top-0 flex items-center justify-center pr-3">
-            <kbd className="pointer-events-none flex h-6 select-none items-center gap-1 rounded border bg-white px-1.5 font-mono text-[14px] font-normal text-muted-foreground opacity-100">
+        <motion.div layout className="rounded-[16px] shadow-none">
+          <div className="relative flex">
+            <motion.input
+              layout
+              ref={inputRef}
+              type="text"
+              className="animated-background h-10 w-[200px] rounded-[14px] border-[1px] border-gray-200 bg-gray-100 px-3 text-sm font-medium text-gray-800 placeholder:text-gray-500 focus:outline-none focus:ring-[2px] focus:ring-gray-700 focus:ring-offset-2 focus:placeholder:opacity-50"
+              placeholder="Search item..."
+              whileFocus={{
+                width: 400,
+              }}
+              transition={{
+                type: 'spring',
+                duration: 0.4,
+                // bounce: 0.25,
+              }}
+              onFocus={() => setIsToggled(!isToggled)}
+              onBlur={() => setIsToggled(!isToggled)}
+            ></motion.input>
+
+            <span className="absolute inset-y-0 right-0 top-0 z-10 flex items-center justify-center gap-1 pr-3">
               <AnimatePresence>
                 {isToggled ? (
-                  <span className="text-[12px]">
-                    <span className="text-[20px]/[20px]"></span>esc
-                  </span>
-                ) : (
-                  <>
-                    <span className="text-[20px]/[20px]">⌘</span>K
-                  </>
-                )}
+                  <motion.span layout className="text-[13px] text-gray-400">
+                    Press
+                  </motion.span>
+                ) : null}
               </AnimatePresence>
-            </kbd>
-          </span>
-        </div>
+              <motion.kbd className="pointer-events-none flex h-6 select-none items-center gap-1 rounded border bg-white px-1.5 font-mono text-[14px] font-normal text-muted-foreground opacity-100">
+                <AnimatePresence>
+                  {isToggled ? (
+                    <span className="text-[12px]">
+                      <span className="text-[20px]/[20px]"></span>esc
+                    </span>
+                  ) : (
+                    <>
+                      <span className="text-[20px]/[20px]">⌘</span>K
+                    </>
+                  )}
+                </AnimatePresence>
+              </motion.kbd>
+              <AnimatePresence>
+                {isToggled ? (
+                  <motion.span layout className="text-[13px] text-gray-400">
+                    to exit
+                  </motion.span>
+                ) : null}
+              </AnimatePresence>
+            </span>
+          </div>
+          <AnimatePresence>
+            {isToggled ? (
+              <motion.div
+                initial={{ height: 0 }}
+                animate={{ height: 'auto' }}
+                exit={{ height: 0 }}
+                transition={{
+                  type: 'spring',
+                  duration: 0.3,
+                }}
+              >
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0, transition: { duration: 0.1 } }}
+                >
+                  <motion.div className="pt-1">
+                    <motion.div>
+                      <motion.p className="bg-white px-2 pb-2 pt-5 text-xs text-gray-400 ">
+                        Recent searches
+                      </motion.p>
+                      <motion.div className="cursor-pointer rounded-[14px] bg-white px-2 py-3 text-base text-gray-700 hover:bg-gray-50">
+                        Recent search item 1
+                      </motion.div>
+                      <hr className="mx-3" />
+                      <motion.div className="cursor-pointer rounded-[14px] bg-white px-2 py-3 text-base text-gray-700 hover:bg-gray-50">
+                        Recent search item 1
+                      </motion.div>
+                      <hr className="mx-3" />
+                      <motion.div className="cursor-pointer rounded-[14px] bg-white px-2 py-3 text-base text-gray-700 hover:bg-gray-50">
+                        Recent search item 1
+                      </motion.div>
+                    </motion.div>
+                  </motion.div>
+                </motion.div>
+              </motion.div>
+            ) : null}
+          </AnimatePresence>
+        </motion.div>
       </Section>
     </>
   )
