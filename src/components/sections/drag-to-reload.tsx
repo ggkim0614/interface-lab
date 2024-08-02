f5389daeaaebca1e971d99de225f92c098c81e7f
George Kim <ggkim0614@gmail.com>
2024-08-02 04:52:26 -0700

    wip

--- src/components/sections/drag-to-reload.tsx
+++ src/components/sections/drag-to-reload.tsx
@@ -2,6 +2,7 @@
 
 import { useState, useEffect } from 'react'
 import {
+  AnimatePresence,
   motion,
   useMotionValue,
   useMotionValueEvent,
@@ -10,10 +11,10 @@ import {
   animate,
   type MotionStyle,
 } from 'framer-motion'
+import { Check } from 'lucide-react'
 import Section from './section'
 
 export default function PullToReload() {
-  const [animationState, setAnimationState] = useState('normal')
   const [atThreshold, setAtThreshold] = useState(false)
   const [segmentLength, setSegmentLength] = useState(20)
   const [runAnimationCycle, setRunAnimationCycle] = useState(false)
@@ -21,6 +22,8 @@ export default function PullToReload() {
   const [displayPercentage, setDisplayPercentage] = useState(0)
   const [logoPosition, setLogoPosition] = useState(0)
   const [svgScale, setSvgScale] = useState(1)
+  const [showCompletion, setShowCompletion] = useState(false)
+  const [animationFinished, setAnimationFinished] = useState(false)
 
   const yDrag = useMotionValue(0)
 
@@ -28,24 +31,20 @@ export default function PullToReload() {
   const pathOpacity = useMotionTemplate`${isAnimationRunning ? 0.2 : yDragOpacity}`
 
   const dragPercentage = useTransform(yDrag, [0, 100], [0, 100])
-
   const logoPositionValue = useTransform(yDrag, [0, 100], [0, 24])
-
   const backgroundColor = useTransform(
     yDrag,
-    [0, 15, 30, 100],
+    [0, 30, 60, 100],
     ['#ffffff', '#ffffff', '#22222', '#50C878']
   )
-
-  const pathPosition = useTransform(yDrag, [0, 90], [20, -180])
+  const pathPosition = useTransform(yDrag, [0, 100], [20, -180])
 
   useMotionValueEvent(yDrag, 'change', (currentY) => {
-    if (currentY > 100 && !atThreshold) {
+    if (currentY > 90 && !atThreshold) {
       setAtThreshold(true)
-      // Trigger the scale effect
       setSvgScale(1.2)
       setTimeout(() => setSvgScale(1), 150)
-    } else if (currentY <= 100) {
+    } else if (currentY <= 90) {
       setAtThreshold(false)
     }
   })
@@ -79,13 +78,9 @@ export default function PullToReload() {
     await animate(pathPosition, -230, { duration: 1 })
     await animate(segmentLength, 230, { duration: 1 })
 
-    // Reset
-    setRunAnimationCycle(false)
-    setAnimationState('normal')
-    setSegmentLength(20)
-    setAtThreshold(false)
-
-    setTimeout(() => setIsAnimationRunning(false), 100)
+    // Animation complete
+    setAnimationFinished(true)
+    setIsAnimationRunning(false)
   }
 
   const handleDragEnd = () => {
@@ -99,6 +94,21 @@ export default function PullToReload() {
       })
     }
   }
+
+  useEffect(() => {
+    let timer: any
+    if (animationFinished) {
+      setShowCompletion(true)
+      timer = setTimeout(() => {
+        setShowCompletion(false)
+        setAnimationFinished(false)
+        setRunAnimationCycle(false)
+        setSegmentLength(20)
+        setAtThreshold(false)
+      }, 2000)
+    }
+    return () => clearTimeout(timer)
+  }, [animationFinished])
 
   useEffect(() => {
     if (runAnimationCycle) {
@@ -167,22 +177,89 @@ export default function PullToReload() {
                   : `${displayPercentage}%`}
             </div>
             <motion.div
-              drag="y" // Allow dragging even during animation
+              drag="y"
+              dragConstraints={{ top: 0, bottom: 100 }}
+              dragElastic={0.5}
               style={{ y: yDrag }}
-              dragElastic={1}
-              dragConstraints={{ top: 0, bottom: 114 }}
-              dragTransition={{
-                bounceStiffness: 1000,
-                bounceDamping: 50,
-                timeConstant: 105,
-              }}
               onDragEnd={handleDragEnd}
-              className="absolute z-50 h-[120px] w-full cursor-pointer items-center justify-center rounded-t-lg border-t-[1px] bg-white text-center font-mono text-gray-500"
+              className="absolute z-50 h-[120px] w-full cursor-pointer items-center justify-center rounded-t-lg border-t-[1px] border-t-gray-200 bg-white text-center font-mono text-gray-500"
             >
-              <div className="flex w-full justify-center pb-2 pt-2">
-                <div className="h-1 w-10 rounded-full bg-gray-300 "></div>
+              <div className="flex w-full justify-center pb-[24px] pt-2">
+                <div className="h-1 w-10 rounded-full bg-gray-200 "></div>
               </div>
-              <span className="select-none">PULL HERE</span>
+              <span className="select-none">
+                <AnimatePresence>
+                  {isAnimationRunning ? (
+                    <motion.div
+                      initial={{ scale: 0.5, opacity: 0, filter: 'blur(4px)' }}
+                      animate={{
+                        scale: 1,
+                        opacity: 1,
+                        filter: 'blur(0px)',
+                        transition: {
+                          delay: 0.2,
+                        },
+                      }}
+                      exit={{
+                        scale: 0.5,
+                        opacity: 0,
+                        filter: 'blur(4px)',
+                        transition: {
+                          duration: 0.1,
+                        },
+                      }}
+                    >
+                      Loading...
+                    </motion.div>
+                  ) : showCompletion ? (
+                    <motion.div
+                      initial={{ scale: 0.5, opacity: 0, filter: 'blur(4px)' }}
+                      animate={{
+                        scale: 1,
+                        opacity: 1,
+                        filter: 'blur(0px)',
+                        transition: {
+                          delay: 0.2,
+                        },
+                      }}
+                      exit={{
+                        scale: 0.5,
+                        opacity: 0,
+                        filter: 'blur(4px)',
+                        transition: {
+                          duration: 0.1,
+                        },
+                      }}
+                      className="flex w-full items-center justify-center"
+                    >
+                      <Check className="h-[18px] w-[14px] pr-[4px] text-green-500" />
+                      Loading complete
+                    </motion.div>
+                  ) : (
+                    <motion.div
+                      initial={{ scale: 0.5, opacity: 0, filter: 'blur(4px)' }}
+                      animate={{
+                        scale: 1,
+                        opacity: 1,
+                        filter: 'blur(0px)',
+                        transition: {
+                          delay: 0.2,
+                        },
+                      }}
+                      exit={{
+                        scale: 0.5,
+                        opacity: 0,
+                        filter: 'blur(4px)',
+                        transition: {
+                          duration: 0.1,
+                        },
+                      }}
+                    >
+                      PULL HERE
+                    </motion.div>
+                  )}
+                </AnimatePresence>
+              </span>
             </motion.div>
           </motion.div>
         </div>
