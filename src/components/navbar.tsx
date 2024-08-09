d1d0bd432ce315b0d5e3c91f776b74403034f95d
George Kim <ggkim0614@gmail.com>
2024-08-09 15:54:36 -0700

    wip

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,8 +2,13 @@ function Navbar() {
   return (
     <div className="flex flex-wrap items-center justify-between gap-4 px-4 py-4">
       <span className="flex gap-2">
-        <div className="font-jbm text-[16px] font-bold leading-tight tracking-tight text-gray-900">
-          INTERFACE LAB.
+        <div className="group font-jbm text-[16px] font-bold leading-tight tracking-tight text-gray-900">
+          I<span className="transition group-hover:text-gray-300">NTER</span>F
+          <span className="transition group-hover:text-gray-300">ACE</span>
+          <span className="opacity-0 transition group-hover:opacity-100">
+            ?
+          </span>
+          LAB.
         </div>
 
         <div className="font-jbm text-[14px] font-medium text-gray-500">
